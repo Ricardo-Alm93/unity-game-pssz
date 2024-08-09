b423fe4f21aef989bc86b44d160cf72a5dfa4ff8
RicardoAlm93 <elthanatos06@gmail.com>
2024-08-09 02:05:46 -0400

    first commit

---
+++ Assets/AllScripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame_Action()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Load the next Scene, in this case the first Map
+    }
+    public void Go_toSettingsMenu_Action() // in a case of create an settings menu_Scene
+    {
+        SceneManager.LoadScene("settingsMenu");
+    }
+
+    public void Go_toMainMenu_Action() // in a case of back to Main menu from an settings menu_Scene
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame_Action()
+    {
+        Application.Quit();
+
+        // the next line also quit in the application if the editor is running
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
