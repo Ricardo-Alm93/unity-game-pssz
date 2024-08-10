793883029474c0da272ed4db3b0758d582838574
RicardoAlm93 <elthanatos06@gmail.com>
2024-08-10 04:36:03 -0400

    Congifuracion de Resolucion y sonido

---
+++ Assets/AllScripts/SettingsManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsManager : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public TMP_Dropdown resolutionDropdown;
+
+
+    Resolution[] resolutions;
+
+    void Start() 
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+        
+        List<String> resolutionOptions_ = new List<String>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string resolutionOption = resolutions[i].width + " x " + resolutions[i].height;
+            resolutionOptions_.Add(resolutionOption);
+
+            if(resolutions[i].width == Screen.currentResolution.width &&
+            resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(resolutionOptions_);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void Set_Volume(float volume)
+    {
+        audioMixer.SetFloat("VolumeMaster_Param", volume);
+    }
+
+    public void Set_Quality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void Set_FullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void Set_Resolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+}
